454c939d0b4ae3ff0eaac1282207ba83d879441e
XploitN1nj4 <hicham.ezzamzami@gmail.com>
2026-07-17 23:35:05 +0100

    initial commit

---
+++ ebankAPI/ebankapp/src/main/java/com/ebank/ebankapp/EbankappApplication.java
@@ -0,0 +1,13 @@
+package com.ebank.ebankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbankappApplication.class, args);
+	}
+
+}
---
+++ ebankAPI/ebankapp/src/test/java/com/ebank/ebankapp/EbankappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebank.ebankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
